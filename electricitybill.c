a20f0a7a2ae73a409509310036c138e76c193308
vvgowtham356 <96586679+vvgowtham356@users.noreply.github.com>
2021-12-31 11:56:29 +0530

    Add files via upload

---
+++ electricitybill.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void main()
+{
+  int customerid,units;
+  float amount,surcharge,netamount;
+  printf("enter the customerid,units");
+  scanf("%d\n%d",&customerid,&units);
+  if (units<200)
+  {
+    amount=units*1.20;
+  	printf("%2f",amount);
+  }
+ else if(units>=200&&units<400)
+ 
+{
+  amount=units*1.50;
+}
+ if(amount>400)
+ {
+   surcharge=amount*15/100;
+  netamount=amount+surcharge;
+  printf("%2f\n%2f\n%2f",amount,surcharge,netamount);
+}
+else if(units>=400&&units<600)
+{
+	amount=units*1.80;
+	surcharge=amount*15/100;
+    netamount=amount+surcharge;
+    printf("%2f\n%2f\n%2f",amount,surcharge,netamount);
+}
+else if(units>=600)
+{
+	amount=units*2.00;
+	surcharge=amount*15/100;
+	netamount=amount+surcharge;
+	printf("%2f\n%2f\n%2f",amount,surcharge,netamount);
+}
+
+}
+
+  
